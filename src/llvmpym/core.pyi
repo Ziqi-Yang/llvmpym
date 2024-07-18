d171f059975e4d31c1bc5a1a248a2e59dde21a1d
Meow King <mr.meowking@anche.no>
2024-07-18 21:01:29 +0800

    feat(core): add some enums

---
+++ src/llvmpym/core.pyi
@@ -0,0 +1,15 @@
+from llvmpym.llvmpym_ext.core import (
+    CallConv as CallConv,
+    DLLStorageClass as DLLStorageClass,
+    IntPredicate as IntPredicate,
+    LandingPadClauseTy as LandingPadClauseTy,
+    Linkage as Linkage,
+    Module as Module,
+    Opcode as Opcode,
+    RealPredicate as RealPredicate,
+    ThreadLocalMode as ThreadLocalMode,
+    TypeKind as TypeKind,
+    UnnamedAddr as UnnamedAddr,
+    ValueKind as ValueKind,
+    Visibility as Visibility
+)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,20 @@
-from llvmpym.core import Module
+from llvmpym.core import *
+
+class TestEnum:
+    def test_enum_member_numbers(self):
+        assert len(Opcode) == 67
+        assert len(TypeKind) == 21
+        assert len(Linkage) == 17
+        assert len(Visibility) == 3
+        assert len(UnnamedAddr) == 3
+        
+    def test_value(self):
+        assert IntPredicate.IntEQ.value == 32
+
+    def test_docstring(self):
+        assert TypeKind.VoidTypeKind.__doc__ == "type with no size"
+
+        
 
 class TestModule:
     def test_module(self):
