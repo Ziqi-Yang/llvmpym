ef6bbf47820c27145073f135e6512922fbee934f
Meow King <mr.meowking@anche.no>
2024-08-04 16:21:44 +0800

    test(ci): specify CXX version for MacOS build

---
+++ src/llvm/Analysis.cpp
@@ -0,0 +1,10 @@
+#include "Analysis.h"
+
+#include <nanobind/nanobind.h>
+
+namespace nb = nanobind;
+using namespace nb::literals;
+
+
+void populateAnalysis(nb::module_ &m);
+
---
+++ src/llvm/Analysis.h
@@ -0,0 +1,9 @@
+#ifndef LLVMPYM_ANALYSIS_H
+#define LLVMPYM_ANALYSIS_H
+
+#include <nanobind/nanobind.h>
+
+void populateAnalysis(nanobind::module_ &m);
+
+
+#endif
